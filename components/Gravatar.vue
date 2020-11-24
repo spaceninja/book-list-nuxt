5bad0aff03dbf1d1b419d572ed74efc9f91fe64d
Scott Vandehey <fake-scott@spaceninja.com>
2020-11-23 17:38:43 -0800

    updated page template

---
+++ components/Gravatar.vue
@@ -0,0 +1,26 @@
+<template><img :src="emailHash" alt="" width="24" height="24" /></template>
+
+<script>
+import crypto from 'crypto';
+export default {
+  props: {
+    email: {
+      type: String,
+      required: true,
+    },
+  },
+  computed: {
+    emailHash() {
+      return `https://www.gravatar.com/avatar/${this.hash(this.email)}`;
+    },
+  },
+  methods: {
+    hash(string) {
+      return crypto
+        .createHash('md5')
+        .update(string.trim().toLowerCase())
+        .digest('hex');
+    },
+  },
+};
+</script>
---
+++ components/WhoAmI.vue
@@ -0,0 +1,30 @@
+<template>
+  <div v-if="isLoggedIn">
+    You are <NuxtLink to="/profile">logged in</NuxtLink> as
+    <Gravatar :email="authUser.email" align="center" />
+    {{ authUser.email }}.
+    <button type="button" @click="logout">Log Out</button>
+  </div>
+  <div v-else>
+    <NuxtLink to="/login">Log In</NuxtLink>
+  </div>
+</template>
+
+<script>
+import { mapState, mapGetters } from 'vuex';
+export default {
+  computed: {
+    ...mapState(['authUser']),
+    ...mapGetters(['isLoggedIn']),
+  },
+  methods: {
+    async logout() {
+      try {
+        await this.$fire.auth.signOut();
+      } catch (e) {
+        alert(e);
+      }
+    },
+  },
+};
+</script>
