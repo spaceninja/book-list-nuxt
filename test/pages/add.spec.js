b73e7f9429923eeaa775a12bdbf0b90781231491
Scott Vandehey <fake-scott@spaceninja.com>
2020-12-07 16:59:30 -0800

    add additional Add page test

--- test/pages/add.spec.js
+++ test/pages/add.spec.js
@@ -1,6 +1,17 @@
 import '@testing-library/jest-dom';
-import { render } from '@testing-library/vue';
+import { render, fireEvent } from '@testing-library/vue';
 import AddPage from '@/pages/add.vue';
+import BookEdit from '@/components/BookEdit.vue';
+import FormInput from '@/components/FormInput.vue';
+
+jest.mock('vuex-map-fields', () => ({
+  getterType: jest.fn(),
+  mapFields: jest.fn(),
+}));
+
+const $router = {
+  push: jest.fn(),
+};
 
 const actions = {
   saveBook: jest.fn(),
@@ -11,6 +22,15 @@ const stubs = {
   BookEdit: {
     template: '<div data-testid="book-edit" />',
   },
+};
+
+const book = {
+  title: 'Book Title',
+  authorFirst: 'John',
+  authorLast: 'Doe',
+  rating: 5,
+  length: 1,
+  isbn: '123',
 };
 
 describe('pages/add.vue', () => {
@@ -30,5 +50,41 @@ describe('pages/add.vue', () => {
       stubs,
     });
     expect(queryByTestId('book-edit')).not.toBeInTheDocument();
+  });
+
+  test('can save book', async () => {
+    const { getByRole, getByLabelText } = render(AddPage, {
+      store: { actions },
+      computed: { isLoggedIn: () => true },
+      stubs: {
+        BookEdit,
+        FormInput,
+      },
+      mocks: {
+        $router,
+      },
+    });
+
+    // mounted lifecycle hook sets book to empty
+    expect(actions.setActiveBook).toHaveBeenCalled();
+
+    // fill out the form
+    const isbn = getByLabelText('ISBN');
+    await fireEvent.update(isbn, book.isbn);
+    const title = getByLabelText('Title');
+    await fireEvent.update(title, book.title);
+    const authorFirst = getByLabelText('Author First Name');
+    await fireEvent.update(authorFirst, book.authorFirst);
+    const authorLast = getByLabelText('Author Last Name');
+    await fireEvent.update(authorLast, book.authorLast);
+    const rating = getByLabelText('Rating');
+    await fireEvent.update(rating, book.rating);
+    const length = getByLabelText('Length');
+    await fireEvent.update(length, book.length);
+
+    // save action trigger
+    await fireEvent.click(getByRole('button'));
+    expect(actions.saveBook).toHaveBeenCalled();
+    expect($router.push).toHaveBeenCalled();
   });
 });
