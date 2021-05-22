7a501fbff14aad3ccb6394fa91afd9a3a5153976
Scott Vandehey <fake-scott@spaceninja.com>
2021-05-22 16:21:36 -0700

    lint fix

--- store/books/getters.js
+++ store/books/getters.js
@@ -7,28 +7,32 @@ export default {
     return book;
   },
 
-  getFilteredBooks: (state) => (filters, allBooks = state.allBooks) => {
-    let bookArray = Object.values(allBooks);
-    filters.forEach((key) => {
-      bookArray = bookArray.filter((book) => book[key]);
-    });
-    return bookArray;
-  },
+  getFilteredBooks:
+    (state) =>
+    (filters, allBooks = state.allBooks) => {
+      let bookArray = Object.values(allBooks);
+      filters.forEach((key) => {
+        bookArray = bookArray.filter((book) => book[key]);
+      });
+      return bookArray;
+    },
 
-  getSortedBooks: (state) => (sort, allBooks = state.allBooks) => {
-    const bookArray = Object.values(allBooks);
-    const firstByOptions = {
-      ignoreCase: true,
-      direction: sort.firstByOrder === 'descending' ? -1 : 0,
-    };
-    const thenByOptions = {
-      ignoreCase: true,
-      direction: sort.thenByOrder === 'descending' ? -1 : 0,
-    };
-    return bookArray.sort(
-      firstBy(sort.firstBy, firstByOptions).thenBy(sort.thenBy, thenByOptions)
-    );
-  },
+  getSortedBooks:
+    (state) =>
+    (sort, allBooks = state.allBooks) => {
+      const bookArray = Object.values(allBooks);
+      const firstByOptions = {
+        ignoreCase: true,
+        direction: sort.firstByOrder === 'descending' ? -1 : 0,
+      };
+      const thenByOptions = {
+        ignoreCase: true,
+        direction: sort.thenByOrder === 'descending' ? -1 : 0,
+      };
+      return bookArray.sort(
+        firstBy(sort.firstBy, firstByOptions).thenBy(sort.thenBy, thenByOptions)
+      );
+    },
 
   getSortedAndFilteredBooks: (state, getters) => (filters, sort) => {
     let bookArray = Object.values(state.allBooks);
